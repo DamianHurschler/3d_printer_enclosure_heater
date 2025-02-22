27f02b7fca43562d1679ba763b5a7c9ef30cde02
Damian Hurschler <damian.hurschler@bluewin.ch>
2025-02-22 23:57:23 +0100

    Added proper startup message.

--- src/main.cpp
+++ src/main.cpp
@@ -58,7 +58,7 @@ void setup() {
   // Display a welcome message on the OLED
   u8g2.clearBuffer();               // clear the internal memory
   u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-  u8g2.drawStr(0, 10, "Hello, World!");  // write something to the internal memory
+  u8g2.drawStr(0, 10, "Starting...");  // write something to the internal memory
   u8g2.setContrast(200);  // Maximum contrast
   u8g2.sendBuffer();                // transfer internal memory to the display
 
